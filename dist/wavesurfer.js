691f735df6177b2f11afc46ad6f7a8de4179918d
matthewtmoran <matthewtmoran@gmail.com>
2017-07-13 10:24:45 -0500

    added bar height param

--- dist/wavesurfer.js
+++ dist/wavesurfer.js
@@ -1425,7 +1425,7 @@ var MultiCanvas = function (_Drawer) {
             var gap = Math.max(this.params.pixelRatio, ~~(bar / 2));
             var step = bar + gap;
 
-            var absmax = 1;
+            var absmax = 1 / this.params.barHeight;
             if (this.params.normalize) {
                 var max = util.max(peaks);
                 var min = util.min(peaks);
@@ -1493,7 +1493,7 @@ var MultiCanvas = function (_Drawer) {
             var offsetY = height * channelIndex || 0;
             var halfH = height / 2;
 
-            var absmax = 1;
+            var absmax = 1 / this.params.barHeight;
             if (this.params.normalize) {
                 var max = util.max(peaks);
                 var min = util.min(peaks);
@@ -3187,6 +3187,7 @@ var WaveSurfer = function (_util$Observer) {
             audioContext: null,
             audioRate: 1,
             autoCenter: true,
+            barHeight: 1,
             backend: 'WebAudio',
             container: null,
             cursorColor: '#333',
